d56864c9af4a5cce6941341eb8eb69a70a4f4351
ilich28 <64256019+ilich28@users.noreply.github.com>
2020-12-02 22:36:17 +0500

    Add files via upload

--- js/neew.js
+++ js/neew.js
@@ -6,7 +6,12 @@ $(document).ready(function(){
   $('.Block_Work_Title').click(function(event) {
     $('.Block_Work_list,.Block_Work_list2').toggleClass('active');
   });
+  $('.header_list').click(function(){ // по нажатию на элемент в меню
+$(".header_burger").addClass("collapsed") //  бургер
+$(".header_menu").removeClass("in") //  меню
 });
+});
+
 new Swiper('.swiper-container', {
   navigation: {
     nextEl: '.swiper-button-next',
@@ -22,9 +27,7 @@ new Swiper('.swiper-container', {
     delay: 3000,
     disableOnInteraction:false,    
   },
-   hashNavigation: {
-    whatchState: true,
-  },
+  
   slidesPerView:2, 
   keyboard: {
     enabled: true,
